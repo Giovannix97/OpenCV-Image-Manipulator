08ddf21cfea312d2d83174f746b326687886a668
Giovannix97 <g.iacovazzo2@studenti.unisa.it>
2020-07-21 15:30:00 +0200

    Implemented all filters

--- Filter.py
+++ Filter.py
@@ -27,3 +27,41 @@ class Filter:
 
     def apply(self):
         pass
+
+
+class AverageFilter(Filter):
+    def __init__(self, image, name, kernel_size):
+        super().__init__(image, name, kernel_size)
+
+    def apply(self):
+        k = self.kernel_size
+        return cv.blur(self.image, (k,k))
+
+
+class GaussianBlur(Filter):
+    def __init__(self, image, name, kernel_size, sigma_x, sigma_y=0):
+        self.sigma_x = sigma_x
+        self.sigma_y = sigma_y
+        super().__init__(image, name, kernel_size)
+
+    def apply(self):
+        k = self.kernel_size
+        return cv.GaussianBlur(self.image, (k,k), self.sigma_x, self.sigma_y )
+
+
+class MedianBlur(Filter):
+    def __init__(self, image, name, kernel_size):
+        super().__init__(image,name,kernel_size)
+
+    def apply(self):
+        return cv.medianBlur(self.image, self.kernel_size)
+
+class BilateralFilter(Filter):
+    def __init__(self, image, name, kernel_size, diameter, sigma_color, sigma_space):
+        self.diameter = diameter
+        self.sigma_color = sigma_color
+        self.sigma_space = sigma_space
+        super().__init__(image,name,kernel_size)
+
+    def apply(self):
+        return cv.bilateralFilter(self.image,self.diameter,self.sigma_color,self.sigma_space)
---
+++ FilterFactory.py
@@ -0,0 +1,16 @@
+
+class FilterFactory:
+    def __init__(self,filter_name):
+        if filter_name.lower() == "average":
+            pass
+        elif filter_name.lower() == "gaussian":
+            pass
+        elif filter_name.lower() == "median":
+            pass
+        elif filter_name.lower() == "bilateral":
+            pass
+        else:
+            pass
+
+
+
